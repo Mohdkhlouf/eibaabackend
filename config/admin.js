cb9f5968de0a6dfeebb3defb273839a6a6e80fe8
Mohammad Khlouf <mohammad.khlouf@gmail.com>
2023-09-09 23:13:52 +0300

    Redner

--- config/admin.js
+++ config/admin.js
@@ -5,6 +5,9 @@ module.exports = ({ env }) => ({
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
+  "netlify-deployments": {
+    enabled: true,
+  },
   transfer: {
     token: {
       salt: env('TRANSFER_TOKEN_SALT'),
